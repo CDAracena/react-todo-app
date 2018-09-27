f76c2108c1f621a04a127011d9bfbf1e31085867
Christian Aracena <CDAracena@gmail.com>
2018-09-27 17:48:31 -0400

    styled header, added font-awesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from './Header';
+import Body from './Body';
+import './index.css';
+import './body.css';
 
 class App extends React.Component {
   state = {
@@ -11,6 +14,7 @@ class App extends React.Component {
     return (
       <div className="main-container">
       <Header/>
+      <Body />
 
     </div>)
   }
---
+++ src/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './body.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+const Body = () => {
+  return (
+    <div className="main-body-container">
+      <span><i className="fa fa-plus-circle"></i> </span>
+
+    </div>
+  )
+}
+
+export default Body;
