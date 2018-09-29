d1abccd0a68837c6af4dfa4e0bb8ee53beb905f3
Christian Aracena <CDAracena@gmail.com>
2018-09-29 18:52:52 -0400

    add and delete todo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import Header from './Header';
+import Body from './Body';
+import './body.css'
 
 class App extends React.Component {
-  state = {
+  constructor(props){
+    super(props);
+  this.state = {
     todos: [],
     count: 0,
-    addMenu: true,
+    text: '',
   }
+this.handleTextChange = this.handleTextChange.bind(this)
+this.handleAddTodo = this.handleAddTodo.bind(this)
+this.removeTodo = this.removeTodo.bind(this)
+this.handleTodoCheckBox = this.handleTodoCheckBox.bind(this)
+this.handleToggle = this.handleToggle.bind(this)
+}
+handleTextChange(event){
+  this.setState({text: event.target.value}, function(){
+  })
+
+}
+
+handleAddTodo(e){
+  let todoId = new Date();
+  let todo = {
+    id: todoId.getTime(),
+    text: this.state.text,
+    completed: false,
+  }
+  this.setState({todos:[...this.state.todos, todo], count: this.state.count + 1},function(){
+  })
+  e.preventDefault();
+}
+
+handleTodoCheckBox(todo){
+    todo.completed = !todo.completed
+
+    this.handleToggle(todo.completed)
+}
+
+
+handleToggle(completed){
+
+if (completed === true){
+  return "todo-strike"
+} else {
+  return "no-strike"
+}
+
+}
+
+
+removeTodo(todo){
+let todos = this.state.todos.filter(function(item){
+  return item !== todo
+})
+  this.setState({todos: todos, count: this.state.count - 1}, function(){
+
+  })
+  console.log(this.state.todos)
+}
   render() {
-    let date = new Date();
     return (
       <div className="main-container">
       <Header/>
+      {this.state.count}
+    <Body inputText={this.handleTextChange} value={this.state.text} addTodo={this.handleAddTodo} todos={this.state.todos} removeTodo={this.removeTodo} todoStatus={this.handleTodoCheckBox} toggleClass={this.handleToggle}/>
 
     </div>)
   }
--- src/Body.js
+++ src/Body.js
@@ -3,11 +3,14 @@ import './body.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 
-const Body = () => {
+const Body = ({inputText, addTodo, todos, todoStatus, removeTodo, toggleClass}) => {
   return (
     <div className="main-body-container">
-      <span className="add-icon"><i className="fa fa-plus-circle "></i> </span>
-
+        <div className="input-boxes">
+          <input type="text" placeholder="Todo Title" className="input-box" onChange={inputText}/>
+          <button onClick={addTodo}> Add </button>
+          {todos.map(todo => <li key={todo.id} className={`${toggleClass}`}><input type="checkbox" onClick={()=> todoStatus(todo)}/> {todo.text} <button onClick={()=> removeTodo(todo)}>Delete </button> </li>)}
+        </div>
     </div>
   )
 }
